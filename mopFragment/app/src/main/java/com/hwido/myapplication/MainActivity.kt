0df0c53ecb6235dcc794cb6e8c9166b79ef37e5a
hwido <hyudojung@naver.com>
2023-11-17 03:27:13 +0900

    mop example

--- mopFragment/app/src/main/java/com/hwido/myapplication/MainActivity.kt
+++ mopFragment/app/src/main/java/com/hwido/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hwido.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import com.google.android.material.tabs.TabLayoutMediator
 import com.hwido.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), SecondFragment.Callbacks {
@@ -15,21 +16,33 @@ class MainActivity : AppCompatActivity(), SecondFragment.Callbacks {
 
         var firFragment = FirstFragment.newInstance("First","2022")
         var secFragment = SecondFragment.newInstance("Second","2022")
-        val fManager = supportFragmentManager
-        fManager.commit {
-            add(binding.fragFrame.id,firFragment)
-            setReorderingAllowed(true)
-            addToBackStack(null)
-        }
 
-        binding.button.setOnClickListener{
-            firFragment.setTitle("New Title for the first Fragment!")
-//            fManager.commit {
-//                replace(binding.fragFrame.id,secFragment)
-//                setReorderingAllowed(true)
-//                addToBackStack(null)
-//            }
-        }
+        val myFrags = listOf(firFragment,secFragment)
+        val fragAdapter = FragmentAdapter(this)
+        fragAdapter.fragList = myFrags
+        binding.viewpager.adapter = fragAdapter
+
+        val tabs = listOf("First","Second")
+        TabLayoutMediator(binding.tabLayout,binding.viewpager){ tab, position ->
+            tab.text = tabs.get(position)
+        }.attach()
+
+
+//        val fManager = supportFragmentManager
+//        fManager.commit {
+//            add(binding.fragFrame.id,firFragment)
+//            setReorderingAllowed(true)
+//            addToBackStack(null)
+//        }
+//
+//        binding.button.setOnClickListener{
+//            firFragment.setTitle("New Title for the first Fragment!")
+////            fManager.commit {
+////                replace(binding.fragFrame.id,secFragment)
+////                setReorderingAllowed(true)
+////                addToBackStack(null)
+////            }
+//        }
 
     }
 
